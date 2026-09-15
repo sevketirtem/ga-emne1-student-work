866c973208e35a6ffed85a652c7d6258d910aeae
sevketirtem <sevketirtem@gmail.com>
2026-09-15 10:35:07 +0200

    Added lesson 3 code oppgave 07_1. Assumption value as parameter, had learned

--- 03-py-functions/02-exercises/07_1_roll_die.py
+++ 03-py-functions/02-exercises/07_1_roll_die.py
@@ -1,11 +1,11 @@
 import random
 def roll_die (sides=50):
      return random.randint(1,sides)
-
+#sides = 50 is a values we assumed when defining the function.
 for i in range(1,11):
     alfa = roll_die()
     print(alfa)
-
+# left parameter empty. Python checked there is no value then it went to assumption value. And run 50 as assumed value !
 for i in range(1,11):
     beta=roll_die()
     print(beta)
